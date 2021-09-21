c76d842a7bbb931b14904b18567c054b429b1b0f
Do Tan Phat <0826240270>
2021-09-21 14:22:25 +0700

    :zap: Main component
    Main component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import TodoList from './components/TodoList'
+import IdleTimer from 'react-idle-timer'
 import './App.css';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      title: '',
-      todoList: [],
+    
+    this.idleTimer = null
+    this.handleOnAction = this.handleOnAction.bind(this)
+    this.handleOnActive = this.handleOnActive.bind(this)
+    this.handleOnIdle = this.handleOnIdle.bind(this)
+
+    if (localStorage.getItem('todoList') === null) {
+      this.state = {
+        title: '',
+        todoList: []
+      }
+    } else {
+      let getlcStorage = localStorage.getItem('todoList');
+      this.state = {
+        title: '',
+        todoList: JSON.parse(getlcStorage)
+      }
     }
+  }
+
+  addStorage = newToDo => {
+      if (localStorage.getItem('todoList') === null) {
+        localStorage.setItem('todoList', JSON.stringify([newToDo]));
+      } else {
+        let lcStorage = localStorage.getItem('todoList');
+        let data = JSON.parse(lcStorage);
+        data.unshift(newToDo);
+        localStorage.setItem('todoList', JSON.stringify(data));
+      }
   }
 
   removeWork = (item) => {
@@ -16,6 +42,11 @@ export default class App extends React.Component {
       const { todoList } = this.state;
       let index = todoList.indexOf(item)
       todoList.splice(index, 1);
+
+      // Remove change complete work in Local Storage
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+
+      // Set state again
       this.setState({ 
         todoList: todoList
       });
@@ -31,6 +62,10 @@ export default class App extends React.Component {
         }
         return item;
       })
+      // Save change complete work in Local Storage
+      localStorage.setItem('todoList', JSON.stringify(todoList));
+
+      // Set state again
       this.setState ({
         todoList: is_complete_Work
       })
@@ -44,27 +79,34 @@ export default class App extends React.Component {
     })
   }
 
-  handleKeyUp = e => {
-    let { todoList } = this.state;
-    let context = e.target.value;
-    let newToDo = {
-      id: Math.floor(Math.random() * 1000),
-      title: context,
-      is_complete: false
-    };
+  handleKeyUp = (e) => {
     if(e.keyCode === 13) {
-      if (todoList.length < 9) {
-        todoList.unshift(newToDo);
-        this.setState ({
-          title: '',
-          todoList: todoList
-        })
-      }
-      else {
-        window.alert('Làm xong hết chưa mà nhập tiếp ? Làm xong nốt đi rồi mở khóa cho nhập tiếp'); 
+      let { todoList } = this.state;
+      let context = e.target.value;
+      let newToDo = {
+        id: Math.floor(Math.random() * 1000),
+        title: context,
+        is_complete: false
+      };
+      if (this.state.title.length === 0) {
         e.preventDefault();
+      } else {
+          if (todoList.length < 9) {
+            // PUT data to localStorage
+            this.addStorage(newToDo);
+
+            todoList.unshift(newToDo);
+            this.setState ({
+              title: '',
+              todoList: todoList
+            })  
+          }
+          else {
+            window.alert('Làm xong hết chưa mà nhập tiếp ? Làm xong nốt đi rồi mở khóa cho nhập tiếp'); 
+            e.preventDefault();
+          }  
+        }
       }
-    }
   }
 
   submit = e => {
@@ -74,8 +116,13 @@ export default class App extends React.Component {
       title: title,
       is_complete: false
     }
-    if (e) {
+    if (e && this.state.title.length === 0) {
+      e.preventDefault();
+    } else {
       if (todoList.length < 9) {
+        // Add data to local storage
+        this.addStorage(newToDo);
+
         todoList.unshift(newToDo);
         this.setState({
           title: '',
@@ -88,10 +135,28 @@ export default class App extends React.Component {
     }
   }
 
+  handleOnAction (event) {
+  }
+
+  handleOnActive (event) {
+    
+  }
+
+  handleOnIdle (event) {
+    window.alert("Có vẻ bạn đang chờ một điều gì đấy! Hãy cho tôi biết khi nào bạn đã sẵn sàng ")
+  }
   render() {
     const { todoList, title } = this.state
     return (
       <div className="App">
+          <IdleTimer
+            ref={ref => { this.idleTimer = ref }}
+            timeout={1000 * 60 * 10}
+            onActive={this.handleOnActive}
+            onIdle={this.handleOnIdle}
+            onAction={this.handleOnAction}
+            debounce={250}
+          />
           <div className="title-text">
             <h3>
               What's the plan for today ?
@@ -111,7 +176,7 @@ export default class App extends React.Component {
             </div>
             {/* Component Todo - List */}
             {
-              todoList.length && todoList.map((item, index) => {
+              todoList.map((item, index) => {
                 return <TodoList 
                 item={item} 
                 key={item.id} 
